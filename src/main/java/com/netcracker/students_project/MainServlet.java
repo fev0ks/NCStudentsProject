f170d5dd468170ba1f5356d3947fc4dc46c9e3b0
mikhail patin <mixa1243.@gmail.com>
2017-03-18 18:54:52 +0300

    start jsp

--- src/main/java/com/netcracker/students_project/MainServlet.java
+++ src/main/java/com/netcracker/students_project/MainServlet.java
@@ -6,16 +6,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
-        PrintWriter out = resp.getWriter();
-        out.print("<h1>Hello Servlet</h1>");
+        req.setAttribute("name", "Test jsp");
+        req.getRequestDispatcher("mypage.jsp").forward(req,resp);
+//        PrintWriter out = resp.getWriter();
+//        out.print("<h1>Hello Servlet</h1>");
     }
 
     @Override
