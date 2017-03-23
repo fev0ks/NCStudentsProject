811d79a4b2eca24b14a48e7de9b301ffdaabc73b
mikhail patin <mixa1243.@gmail.com>
2017-03-23 14:25:31 +0300

    added null CUID

--- src/main/java/com/netcracker/students_project/dao/TaskTegDao.java
+++ src/main/java/com/netcracker/students_project/dao/TaskTegDao.java
@@ -1,4 +1,32 @@
 package com.netcracker.students_project.dao;
 
-public class TaskTegDao {
+import com.netcracker.students_project.entity.TaskTegEntity;
+import com.netcracker.students_project.entity.exception.ExceptionDao;
+import com.netcracker.students_project.interfaces.IDao;
+
+public class TaskTegDao implements IDao<TaskTegEntity>{
+    @Override
+    public void get(TaskTegEntity entity) throws ExceptionDao {
+
+    }
+
+    @Override
+    public TaskTegEntity create(TaskTegEntity entity) throws ExceptionDao {
+        return null;
+    }
+
+    @Override
+    public void update(TaskTegEntity entity) throws ExceptionDao {
+
+    }
+
+    @Override
+    public void delete(TaskTegEntity entity) throws ExceptionDao {
+
+    }
+
+    @Override
+    public String toString(TaskTegEntity entity) {
+        return null;
+    }
 }
--- src/main/java/com/netcracker/students_project/dao/TegDao.java
+++ src/main/java/com/netcracker/students_project/dao/TegDao.java
@@ -1,4 +1,32 @@
 package com.netcracker.students_project.dao;
 
-public class TegDao {
+import com.netcracker.students_project.entity.TegEntity;
+import com.netcracker.students_project.entity.exception.ExceptionDao;
+import com.netcracker.students_project.interfaces.IDao;
+
+public class TegDao implements IDao<TegEntity>{
+    @Override
+    public void get(TegEntity entity) throws ExceptionDao {
+        
+    }
+
+    @Override
+    public TegEntity create(TegEntity entity) throws ExceptionDao {
+        return null;
+    }
+
+    @Override
+    public void update(TegEntity entity) throws ExceptionDao {
+
+    }
+
+    @Override
+    public void delete(TegEntity entity) throws ExceptionDao {
+
+    }
+
+    @Override
+    public String toString(TegEntity entity) {
+        return null;
+    }
 }
