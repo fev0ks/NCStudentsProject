4c9cafb6245e105acddafaf57a396dd2790890fe
Dmitry28 <dmitry.parshikov28@gmail.com>
2017-03-28 14:53:06 +0300

    add Entity and change hibernate.cfg

---
+++ src/main/java/com/netcracker/students_project/entity/CommentTask.java
@@ -0,0 +1,67 @@
+package com.netcracker.students_project.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "comments_task", schema = "public", catalog = "nc_stud_proj")
+public class CommentTask {
+
+    private long id;
+    private String text;
+    private Date dtCreated;
+
+    private User user;
+    private Task task;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "serial")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "text")
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Basic
+    @Column(name = "dt_created")
+    public Date getDtCreated() {
+        return dtCreated;
+    }
+
+    public void setDtCreated(Date dtCreated) {
+        this.dtCreated = dtCreated;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id")
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+}
